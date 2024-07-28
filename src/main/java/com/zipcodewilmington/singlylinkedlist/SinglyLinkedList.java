18442b4bc3a401a2d13e37fe69b84e26532aa8f1
igordon215 <igordon215@gmail.com>
2024-07-27 23:28:22 -0400

    addNode

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,31 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<Type extends Comparable<Type>> {
+    public Node<Type> head = null;
+    public Node<Type> tail = null;
+
+    public void addNode(Type data) {
+        Node<Type> newNode = new Node<Type>(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+        tail = newNode;
+    }
+
+
+    public int size() {
+        int counter = 0;
+        Node<Type> current = head;
+
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+            counter++;
+        }
+        return counter;
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,26 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void addNodeTest() {
+        //Given:
+        SinglyLinkedList<Integer> singLL = new SinglyLinkedList<Integer>();
+
+        //When:
+        singLL.addNode(5);
+        singLL.addNode(2);
+        singLL.addNode(3);
+
+        //Then:
+        Assert.assertEquals(3, singLL.size());
+    }
+
+
 }
